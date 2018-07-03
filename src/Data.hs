8f591b1289726607e47ea1875ed04db7f1ca00ae
Christian Panadero <panavtec@gmail.com>
2018-07-03 13:54:19 +0100

    add dependencies

--- src/Data.hs
+++ src/Data.hs
@@ -16,7 +16,7 @@ instance ToJSON User
 users :: [User]
 users =
     [ User "Goku"         9001 (fromGregorian 1683 3 1)
-    , User "Vegeta"       9000 (fromGregorian 1905 12 1)
+    , User "Vegeta"       9000 (fromGregorian 1905 1 1)
     , User "Gohan"        5000 (fromGregorian 0195 2 21)
     , User "Piccolo"      4500 (fromGregorian 1925 2 20)
     , User "Krillin"      3500 (fromGregorian 1905 2 22)
